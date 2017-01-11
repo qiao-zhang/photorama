c45ec3087f48a4cfafb40d5398aad4daa6e842fa
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-11 13:55:02 -0500

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Qiao Zhang on 1/11/17.
+//  Copyright © 2017 Qiao Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
