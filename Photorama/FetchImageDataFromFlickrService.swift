f18f966ac933751815b2c110cae6fd1ca845d9e4
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-02-16 19:39:15 -0500

    add service for fetching image data from Flickr API & using URLSession API

---
+++ Photorama/FetchImageDataFromFlickrService.swift
@@ -0,0 +1,39 @@
+//
+// Created by Qiao Zhang on 2/16/17.
+// Copyright (c) 2017 Qiao Zhang. All rights reserved.
+//
+
+import Foundation
+
+class FetchImageDataFromFlickrService: FetchImageDataService {
+  
+  private var currentFetching: URLSessionDataTask?
+  private let session: URLSession = {
+    let config = URLSessionConfiguration.default
+    return URLSession(configuration: config)
+  }()
+  
+  func cancelCurrentFetching() {
+    currentFetching?.cancel()
+  }
+
+  func fetchImageDataAsync(
+      of url: URL,
+      completion: @escaping (FetchImageDataResult) -> Void) {
+    let request = URLRequest(url: url)
+    currentFetching = session.dataTask(with: request) { data, response, error in
+      let result: FetchImageDataResult
+      if let data = data {
+        result = .success(data)
+      } else if let error = error as? NSError {
+        if error.domain == NSURLErrorDomain,
+           error.code == NSURLErrorCancelled {
+          result = .failure(.cancelled)
+        }
+        result = .failure(.other(error))
+      }
+    }
+    currentFetching!.resume()
+  }
+
+}
